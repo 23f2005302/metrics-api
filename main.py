2e52766ada25a1a397f200eb96243202e9e8e408
23f2005302 <23f2005302@ds.study.iitm.ac.in>
2026-07-08 10:54:10 +0530

    Fix formatting issues in main.py

--- main.py
+++ main.py
@@ -61,15 +61,15 @@ def get_stats(values: str):
 ISSUER = "https://idp.exam.local"
 AUDIENCE = "tds-0vwph3az.apps.exam.local"
 
-# FIX: Bulletproof formatting to prevent hidden space errors in GitHub
+# THE FIX: This is the exact key you just pasted, formatted perfectly to prevent hidden spaces!
 PUBLIC_KEY = (
     "-----BEGIN PUBLIC KEY-----\n"
     "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY\n"
-    "cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbyGuc3EbNy4xFyf5Cbj5DHJMID\n"
+    "cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID\n"
     "EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc\n"
-    "WyjJG2axVfmq7i6SuKr1JoWYG7xTTAvKPujS140tsQfO3h5NepzdfXpr28oNnzfW\n"
-    "ed+zc1R6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd119VDetJZVEgC5tkyvXsfI\n"
-    "SI6iyrYbkR0NEBSqq4XkadEjsCs4F1RncsS4L1gniT7GlkL9Mce3b0wGLs9/7ZIX\n"
+    "WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW\n"
+    "ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI\n"
+    "SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX\n"
     "dQIDAQAB\n"
     "-----END PUBLIC KEY-----"
 )
